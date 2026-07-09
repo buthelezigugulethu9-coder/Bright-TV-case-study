366e871d7a54aea58ebcab98105796bc1abef7b9
buthelezigugulethu9-coder <buthelezigugulethu9@gmail.com>
2026-07-09 09:10:51 +0000

    Bright TV coding

--- Bright TV.sql
+++ Bright TV.sql
@@ -1,5 +1,24 @@
 -- Databricks notebook source
-SELECT*
+SELECT *
+FROM brighttv.dataset.userprofiles;
+
+SELECT DISTINCT gender
+FROM brighttv.dataset.userprofiles;
+
+SELECT DISTINCT
+CASE
+   WHEN gender='None' THEN 'Unknown'
+   WHEN gender=' ' THEN 'Unknown'
+   WHEN gender IS NULL THEN 'Unknown'
+END AS gender
 FROM brighttv.dataset.userprofiles
+
+
+
+
+
+
+
+
 
 
